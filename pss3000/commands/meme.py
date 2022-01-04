fcf2dae8036d4d88b08862367a1064d4f5fd5725
ap <47916875+atomicparade@users.noreply.github.com>
2022-01-04 18:50:31 -0500

    Add skeleton for option processing for meme create

--- pss3000/commands/meme.py
+++ pss3000/commands/meme.py
@@ -97,6 +97,8 @@ blur_options = StrOptions(
     "Create a meme",
     default_to_ephemeral=False,
 )
+# pylint: disable-next=too-many-arguments
+# pylint: disable-next=too-many-branches
 async def command_meme_create(
     ctx: tanjun.abc.SlashContext,
     image_url_or_template_name: str,
@@ -109,8 +111,43 @@ async def command_meme_create(
     blur: str,
 ) -> None:
     """Create a meme."""
+    meme_command = f"create_meme({image_url_or_template_name})"
+
+    if flip == flip_options.flip_horizontally:
+        meme_command = f"{meme_command} +flip_horizontally"
+    elif flip == flip_options.flip_vertically:
+        meme_command = f"{meme_command} +flip_vertically"
+    elif flip == flip_options.flip_both_horizontally_and_vertically:
+        meme_command = f"{meme_command} +flip_both_horizontally_and_vertically"
+
+    if make_square == make_square_options.squish_into_a_square:
+        meme_command = f"{meme_command} +squish_into_a_square"
+
+    if invert_colours == invert_colours_options.invert_colours:
+        meme_command = f"{meme_command} +invert_colours"
+
+    if make_monochrome == make_monochrome_options.make_monochrome:
+        meme_command = f"{meme_command} +make_monochrome"
+
+    if rotate != 0:
+        meme_command = f"{meme_command} +rotate({rotate})"
+
+    if deep_fry == deep_fry_options.just_a_little:
+        meme_command = f"{meme_command} +deep_fry.just_a_little"
+    elif deep_fry == deep_fry_options.thats_a_lot_of_oil:
+        meme_command = f"{meme_command} +deep_fry.thats_a_lot_of_oil"
+    elif deep_fry == deep_fry_options.very_burnt:
+        meme_command = f"{meme_command} +deep_fry.very_burnt"
+
+    if blur == blur_options.just_a_little:
+        meme_command = f"{meme_command} +blur.just_a_little"
+    elif blur == blur_options.where_are_my_glasses:
+        meme_command = f"{meme_command} +blur.where_are_my_glasses"
+    elif blur == blur_options.help:
+        meme_command = f"{meme_command} +blur.help"
+
     try:
-        await ctx.respond("meme")
+        await ctx.respond(meme_command)
     except (hikari.ForbiddenError, hikari.NotFoundError):
         pass
 
